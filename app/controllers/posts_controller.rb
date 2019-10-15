a4c6ebbd6c77071aa68396ccaa34127836ee2381
Reina <r.clarkemitchell@gmail.com>
2019-10-15 17:13:03 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,4 +12,18 @@ class PostsController < ApplicationController
   end
 
   # add create method here
+  def create
+      @post = Post.create(title: params[:title], description: params[:description])
+      redirect_to post_path(@post)
+  end
+
+  private
+
+  # def params_form
+  #   # byebug
+  #   params.require(:post).permit(:title, :description)
+  #
+  # end
+
+
 end
